09075a73211c9383fddff3c511b832e475cd85e5
shouydev <shouyestefanero@gmail.com>
2026-04-23 13:57:55 -0500

    feat(header): add header component with logo and buttons.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,8 +1,11 @@
 import { Component, signal } from '@angular/core';
+import {Header} from './shared/presentation/components/header/header';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [
+    Header
+  ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
