02fe62f38ff70e5e6a001d39677400b994aab047
shouydev <shouyestefanero@gmail.com>
2026-04-26 07:02:57 -0500

    fix: move footer files

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,7 +4,7 @@ import {Hero} from './value-proposition/presentation/components/hero/hero';
 import { Card } from "./benefits/presentation/components/card/card";
 import { Pricing } from "./pricing/presentation/components/pricing/pricing";
 import { Team } from "./team/presentation/components/team/team";
-import { Footer } from "./footer/presentation/components/footer/footer";
+import { Footer } from "./shared/presentation/components/footer/footer";
 
 @Component({
   selector: 'app-root',
