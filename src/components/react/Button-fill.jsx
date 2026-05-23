2e426427df60d2199c66bbe19e0fdda7c3b5ba37
shouydev <shouyestefanero@gmail.com>
2026-05-23 18:48:21 -0500

    style(value-proposition): add color and css effects.

--- src/components/react/Button-fill.jsx
+++
@@ -1,7 +0,0 @@
-export default function ButtonFill({title}) {
-    return (
-        <a class="rounded-md text-white text-center transition-all ease-in-out hover:-translate-y-0 min-w-[250px] px-10 py-3 md:py-4 bg-[#0071eb] hover:bg-[#004FA6] mt-6 mb-4 font-[Arimo] font-black" href="#">
-            <span class="whitespace-nowrap headline headline--6">{title}</span>
-        </a>
-    );
-}
